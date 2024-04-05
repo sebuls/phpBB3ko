d83600b507d23b7ba7915529de4467963832ff91
세벌 <sebuls@gmail.com>
2024-04-05 21:25:38 +0900

    cli.php index.htm

--- cli.php
+++ cli.php
@@ -35,40 +35,40 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-	'CLI_APCU_CACHE_NOTICE'				=> 'APCu cache는 관리 제어판을 통해 지워야 합니다.',
+	'CLI_APCU_CACHE_NOTICE'			=> 'APCu cache는 관리 제어판을 통해 지워야 합니다.',
 
-	'CLI_CONFIG_CANNOT_CACHED'			=> '이 옵션은 설정 옵션이 너무 자주 바뀌어서 효율적으로 캐시 되야 할 때 설정하십시오.',
-	'CLI_CONFIG_CURRENT'				=> '현재 설정값, 0과 1은 부울 값',
-	'CLI_CONFIG_DELETE_SUCCESS'			=> '성공적 삭제 config %s.',
-	'CLI_CONFIG_NEW'				=> '새 설정값, 0과 1은 부울 값',
-	'CLI_CONFIG_NOT_EXISTS'				=> 'Config %s 없음',
-	'CLI_CONFIG_OPTION_NAME'			=> '설정 옵션의 이름',
+	'CLI_CONFIG_CANNOT_CACHED'		=> '이 옵션은 설정 옵션이 너무 자주 바뀌어서 효율적으로 캐시 되야 할 때 설정하십시오.',
+	'CLI_CONFIG_CURRENT'			=> '현재 설정값, 0과 1은 부울 값',
+	'CLI_CONFIG_DELETE_SUCCESS'		=> '성공적 삭제 config %s.',
+	'CLI_CONFIG_NEW'			=> '새 설정값, 0과 1은 부울 값',
+	'CLI_CONFIG_NOT_EXISTS'			=> 'Config %s 없음',
+	'CLI_CONFIG_OPTION_NAME'		=> '설정 옵션의 이름',
 	'CLI_CONFIG_PRINT_WITHOUT_NEWLINE'	=> '끝에 새 행 없이 값이 출력 되려면 이 옵션을 설정하십시오.',
-	'CLI_CONFIG_INCREMENT_BY'			=> '증분 값',
+	'CLI_CONFIG_INCREMENT_BY'		=> '증분 값',
 	'CLI_CONFIG_INCREMENT_SUCCESS'		=> '설정 %s를 증가했습니다',
-	'CLI_CONFIG_SET_FAILURE'			=> '설정 %s 할 수 없습니다',
-	'CLI_CONFIG_SET_SUCCESS'			=> '설정 %s 했습니다',
+	'CLI_CONFIG_SET_FAILURE'		=> '설정 %s 할 수 없습니다',
+	'CLI_CONFIG_SET_SUCCESS'		=> '설정 %s 했습니다',
 
-	'CLI_DESCRIPTION_CRON_LIST'			=> '준비 및 미 준비 cron job을 출력.',
-	'CLI_DESCRIPTION_CRON_RUN'			=> '준비된 cron task 모두 실행.',
-	'CLI_DESCRIPTION_CRON_RUN_ARGUMENT_1'		=> '실행할 태스크 이름',
-	'CLI_DESCRIPTION_DB_LIST'			=> '설치되고 가능한 마이그레이션 목록.',
-	'CLI_DESCRIPTION_DB_MIGRATE'			=> '마이그레이션 적용하여 데이터베이스 업데이트.',
-	'CLI_DESCRIPTION_DB_REVERT'			=> '마이그레이션 되돌림.',
-	'CLI_DESCRIPTION_DELETE_CONFIG'			=> '설정 옵션 지움',
-	'CLI_DESCRIPTION_DISABLE_EXTENSION'		=> '명시된 확장 비활성.',
-	'CLI_DESCRIPTION_ENABLE_EXTENSION'		=> '명시된 확장 활성.',
-	'CLI_DESCRIPTION_FIND_MIGRATIONS'		=> '의존하지 않는 마이그레이션을 검색.',
-	'CLI_DESCRIPTION_FIX_LEFT_RIGHT_IDS'		=> '포럼 및 모듈 트리 구조 복구.',
-	'CLI_DESCRIPTION_GET_CONFIG'			=> '설정 옵션 값 얻기',
-	'CLI_DESCRIPTION_INCREMENT_CONFIG'		=> '설정 옵션 정수값 증가',
-	'CLI_DESCRIPTION_LIST_EXTENSIONS'		=> '데이터베이스 및 파일 확장 모두 나열.',
+	'CLI_DESCRIPTION_CRON_LIST'		=> '준비 및 미 준비 cron job을 출력.',
+	'CLI_DESCRIPTION_CRON_RUN'		=> '준비된 cron task 모두 실행.',
+	'CLI_DESCRIPTION_CRON_RUN_ARGUMENT_1'	=> '실행할 태스크 이름',
+	'CLI_DESCRIPTION_DB_LIST'		=> '설치되고 가능한 마이그레이션 목록.',
+	'CLI_DESCRIPTION_DB_MIGRATE'		=> '마이그레이션 적용하여 데이터베이스 업데이트.',
+	'CLI_DESCRIPTION_DB_REVERT'		=> '마이그레이션 되돌림.',
+	'CLI_DESCRIPTION_DELETE_CONFIG'		=> '설정 옵션 지움',
+	'CLI_DESCRIPTION_DISABLE_EXTENSION'	=> '명시된 확장 비활성.',
+	'CLI_DESCRIPTION_ENABLE_EXTENSION'	=> '명시된 확장 활성.',
+	'CLI_DESCRIPTION_FIND_MIGRATIONS'	=> '의존하지 않는 마이그레이션을 검색.',
+	'CLI_DESCRIPTION_FIX_LEFT_RIGHT_IDS'	=> '포럼 및 모듈 트리 구조 복구.',
+	'CLI_DESCRIPTION_GET_CONFIG'		=> '설정 옵션 값 얻기',
+	'CLI_DESCRIPTION_INCREMENT_CONFIG'	=> '설정 옵션 정수값 증가',
+	'CLI_DESCRIPTION_LIST_EXTENSIONS'	=> '데이터베이스 및 파일 확장 모두 나열.',
 
-	'CLI_DESCRIPTION_OPTION_ENV'			=> '환경몀.',
-	'CLI_DESCRIPTION_OPTION_SAFE_MODE'		=> '안전 모드 (확장 없음)에서 실행.',
-	'CLI_DESCRIPTION_OPTION_SHELL'			=> '셸 시작.',
+	'CLI_DESCRIPTION_OPTION_ENV'		=> '환경명.',
+	'CLI_DESCRIPTION_OPTION_SAFE_MODE'	=> '안전 모드 (확장 없음)에서 실행.',
+	'CLI_DESCRIPTION_OPTION_SHELL'		=> '셸 시작.',
 
-	'CLI_DESCRIPTION_PURGE_EXTENSION'		=> '명시된 확장 제거.',
+	'CLI_DESCRIPTION_PURGE_EXTENSION'	=> '명시된 확장 제거.',
 
 	'CLI_DESCRIPTION_REPARSER_LIST'			=> '재분석 가능한 텍스트 유형 목록.',
 	'CLI_DESCRIPTION_REPARSER_AVAILABLE'			=> '가능한 재분석기:',
@@ -92,7 +92,7 @@ $lang = array_merge($lang, array(
 	'CLI_DESCRIPTION_UPDATE_CHECK_OPTION_CACHE'		=> '캐시 갖고 check 명령 실행.',
 	'CLI_DESCRIPTION_UPDATE_CHECK_OPTION_STABILITY'	=> '안정 또는 불안정 버전만 확인하도록 선택하여 명령 실행.',
 
-	'CLI_DESCRIPTION_UPDATE_HASH_BCRYPT'		=> '오래된 패스워드를 업데이트 해서 bcrypt로 해시되게 합니다.',
+	'CLI_DESCRIPTION_UPDATE_HASH_BCRYPT'		=> '오래된 암호를 업데이트 해서 bcrypt로 해시되게 합니다.',
 
 	'CLI_ERROR_INVALID_STABILITY' => '"%s"는 "안정" 또는 "불안정"으로 설정합니다.',
 
@@ -103,7 +103,7 @@ $lang = array_merge($lang, array(
 	'CLI_DESCRIPTION_USER_ACTIVATE_INACTIVE'	=> '사용자는 이미 비활성되었습니다.',
 	'CLI_DESCRIPTION_USER_ADD'			=> '사용자 추가.',
 	'CLI_DESCRIPTION_USER_ADD_OPTION_USERNAME'	=> '사용자 이름',
-	'CLI_DESCRIPTION_USER_ADD_OPTION_PASSWORD'	=> '사용자 패스워드',
+	'CLI_DESCRIPTION_USER_ADD_OPTION_PASSWORD'	=> '사용자 암호',
 	'CLI_DESCRIPTION_USER_ADD_OPTION_EMAIL'		=> '사용자의 E-mail 주소',
 	'CLI_DESCRIPTION_USER_ADD_OPTION_NOTIFY'	=> '계정 활성화 메일을 사용자에게 보내기 (기본값은 보내지 않음)',
 	'CLI_DESCRIPTION_USER_DELETE'			=> '사용자 계정 삭제.',
@@ -125,22 +125,22 @@ $lang = array_merge($lang, array(
 	'CLI_EXTENSION_UPDATE_SUCCESS'		=> '확장 %s를 업데이트 했습니다',
 	'CLI_EXTENSION_NOT_FOUND'		=> '확장을 못 찾았습니다.',
 	'CLI_EXTENSION_NOT_ENABLEABLE'		=> '확장 %s는 활성화 안 됨니다.',
-	'CLI_EXTENSIONS_AVAILABLE'			=> '사용가능',
-	'CLI_EXTENSIONS_DISABLED'			=> '비활성화',
-	'CLI_EXTENSIONS_ENABLED'			=> '활성화',
+	'CLI_EXTENSIONS_AVAILABLE'		=> '사용가능',
+	'CLI_EXTENSIONS_DISABLED'		=> '비활성화',
+	'CLI_EXTENSIONS_ENABLED'		=> '활성화',
 
-	'CLI_FIXUP_FIX_LEFT_RIGHT_IDS_SUCCESS'		=> '포럼과 모듈의 트리 구조를 복구했습니다.',
-	'CLI_FIXUP_UPDATE_HASH_BCRYPT_SUCCESS'		=> '오래된 패스워드 해시를 bcrypt로 복구 했습니다.',
+	'CLI_FIXUP_FIX_LEFT_RIGHT_IDS_SUCCESS'	=> '포럼과 모듈의 트리 구조를 복구했습니다.',
+	'CLI_FIXUP_UPDATE_HASH_BCRYPT_SUCCESS'	=> '오래된 암호 해시를 bcrypt로 복구 했습니다.',
 
-	'CLI_MIGRATION_NAME'				=> '마이그레이션 이름, 네임스페이스 포함 (슬래시를 백슬래시 대신 사용하여 문제를 피하세요).',
-	'CLI_MIGRATIONS_AVAILABLE'			=> '가능한 마이그레이션',
-	'CLI_MIGRATIONS_INSTALLED'			=> '설치된 마이그레이션',
-	'CLI_MIGRATIONS_ONLY_AVAILABLE'		    => '가능한 마이그레이션만 보이기',
+	'CLI_MIGRATION_NAME'			=> '마이그레이션 이름, 네임스페이스 포함 (슬래시를 백슬래시 대신 사용하여 문제를 피하세요).',
+	'CLI_MIGRATIONS_AVAILABLE'		=> '가능한 마이그레이션',
+	'CLI_MIGRATIONS_INSTALLED'		=> '설치된 마이그레이션',
+	'CLI_MIGRATIONS_ONLY_AVAILABLE'		=> '가능한 마이그레이션만 보이기',
 	'CLI_MIGRATIONS_EMPTY'                  => '마이그레이션 없음.',
 
-	'CLI_REPARSER_REPARSE_REPARSING'		=> '%1$s 재분석(범위 %2$d..%3$d)',
+	'CLI_REPARSER_REPARSE_REPARSING'	=> '%1$s 재분석(범위 %2$d..%3$d)',
 	'CLI_REPARSER_REPARSE_REPARSING_START'	=> '재분석 %s...',
-	'CLI_REPARSER_REPARSE_SUCCESS'			=> '재분석 성공',
+	'CLI_REPARSER_REPARSE_SUCCESS'		=> '재분석 성공',
 
 	// In all the case %1$s is the logical name of the file and %2$s the real name on the filesystem
 	// eg: big_image.png (2_a51529ae7932008cf8454a95af84cacd) generated.
@@ -167,11 +167,11 @@ $lang = array_merge($lang, array(
 
 // Additional help for commands.
 $lang = array_merge($lang, array(
-	'CLI_HELP_CRON_RUN'			=> $lang['CLI_DESCRIPTION_CRON_RUN'] . ' Optionally you can specify a cron task name to run only the specified cron task.',
-	'CLI_HELP_USER_ACTIVATE'	=> 'Activate a user account, or deactivate an account using the <info>--deactivate</info> option.
-To optionally send an activation email to the user, use the <info>--send-email</info> option.',
-	'CLI_HELP_USER_ADD'			=> 'The <info>%command.name%</info> command adds a new user:
-If this command is run without options, you will be prompted to enter them.
-To optionally send an email to the new user, use the <info>--send-email</info> option.',
+	'CLI_HELP_CRON_RUN'		=> $lang['CLI_DESCRIPTION_CRON_RUN'] . '선택적으로 지정된 cron 태스크만 실행하도록 cron 태스크 이름을 지정할 수 있습니다.',
+	'CLI_HELP_USER_ACTIVATE'	=> '사용자 계정을 활성화 또는 비활성화 <info>--deactivate</info> 옵션.사용자에게 활성화 이메일을 보내려면 <info>--send-email</info> 옵션을 사용합니다. 
+사용자에게 활성화 이메일을 보내려면 <info>--send-email</info> 옵션을 사용합니다.',
+	'CLI_HELP_USER_ADD'		=> '<info>%command.name%</info> 명령은 사용자를 추가:
+이 명령을 옵션 없이 실행하면, 그것을 입력하라고 할 겁니다.
+선택적으로 새 사용자에게 메일 보내려면, <info>--send-email</info> 옵션을 쓰세요.',
 	'CLI_HELP_USER_RECLEAN'		=> 'Re-clean usernames will check all stored usernames and ensure clean versions are also stored. Cleaned usernames are a case insensitive form, NFC normalized and transformed to ASCII.',
 ));
